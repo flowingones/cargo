83bd72abf15490d27c2d002abebf4a01b1808380
Daniel Steuri <daniel.steuri@flowingones.ch>
2021-11-18 10:09:38 +0000

    - Remove the DEFAULT part to the const name - Fix typo

--- framework/core/constants.ts
+++ framework/core/constants.ts
@@ -1,4 +1,4 @@
 export const NAME = "Cargo";
 export const CARGO_VERSION = "0.1.0";
-export const CARGO_DEFAULT_PORT = 8000;
-export const CARGO_ROUTE_DIRECTORY = "routes";
+export const CARGO_PORT = 8000;
+export const CARGO_ROUTES_DIRECTORY = "routes";
