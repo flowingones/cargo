dc3203d5c756132f6fe9244048e655f811f8b3a0
Daniel Steuri <daniel.steuri@flowingones.ch>
2021-11-20 20:29:23 +0000

    Implementation of route grouping.

--- framework/core/routing/group.ts
+++ framework/core/routing/group.ts
@@ -2,15 +2,20 @@ import { Middleware } from "../middleware/middleware.ts";
 import { Route } from "../routing/route.ts";
 
 export class RouteGroup {
-  path?: URLPattern;
+  prefix: URLPattern;
   chain: Middleware[] = [];
   routes: Route[] = [];
 
-  constructor(path: string) {
-    this.path = new URLPattern({ pathname: path });
+  link = this.middleware;
+
+  constructor(prefix: string, route?: Route[]) {
+    this.prefix = new URLPattern({ pathname: prefix });
+    if (route) {
+      this.route(route);
+    }
   }
 
-  link(middleware: Middleware | Middleware[]): RouteGroup {
+  middleware(middleware: Middleware | Middleware[]): RouteGroup {
     if (middleware instanceof Array) {
       for (const eachMiddleware of middleware) {
         this.chain.push(eachMiddleware);
@@ -18,21 +23,40 @@ export class RouteGroup {
     } else {
       this.chain.push(middleware);
     }
+    this.routes.forEach((toRoute) => {
+      this.prependMiddleware(toRoute);
+    });
     return this;
   }
 
-  route(routes: Route | Route[]): RouteGroup {
-    if (routes instanceof Array) {
-      for (const route of routes) {
-        this.routes.push(route);
+  route(toRoute: Route | Route[]): RouteGroup {
+    if (toRoute instanceof Array) {
+      for (const route of toRoute) {
+        this.routes.push(this.prepare(route));
       }
     } else {
-      this.routes.push(routes);
+      this.routes.push(this.prepare(toRoute));
     }
     return this;
   }
+
+  private prepare(toRoute: Route): Route {
+    this.prefixRoute(toRoute);
+    this.prependMiddleware(toRoute);
+    return toRoute;
+  }
+
+  private prefixRoute(toRoute: Route): void {
+    toRoute.path = new URLPattern({
+      pathname: `${this.prefix.pathname}${toRoute.path.pathname}`,
+    });
+  }
+
+  private prependMiddleware(toRoute: Route): void {
+    toRoute.chain = [...this.chain, ...toRoute.chain];
+  }
 }
 
-export function Group(path: string): RouteGroup {
-  return new RouteGroup(path);
+export function Group(path?: string, routes?: Route[]): RouteGroup {
+  return new RouteGroup(path || "", routes);
 }
