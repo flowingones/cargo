6551d1c1e023cc8f0b00f193e2e119cc7e8bddd8
Daniel Steuri <daniel.steuri@flowingones.ch>
2021-12-05 14:59:43 +0000

    Add comments

--- examples/hello-world.ts
+++ examples/hello-world.ts
@@ -1,17 +1,25 @@
-import {
-  bootstrap,
-  Get,
-  RequestContext,
-} from "https://deno.land/x/cargo@0.1.4/core/mod.ts";
+import { bootstrap, Get } from "https://deno.land/x/cargo@0.1.4/core/mod.ts";
 import { measure } from "https://deno.land/x/cargo@0.1.4/middleware/mod.ts";
 
 interface MessageParams {
   message: string;
 }
 
+/*
+ * 1. Register Route and Request Handler
+ */
 Get("/:message", ({ params }) => {
+  /*
+   * 2. Return Response
+   */
   return new Response(`${(<MessageParams> params).message}`);
-}).middleware(measure);
+})
+  /*
+   * 3. Apply middleware functions (optional)
+   */
+  .middleware(measure);
 
-const App = await bootstrap();
-App.run();
+/*
+ * 4. Bootstrap and Run the Application
+ */
+(await bootstrap()).run();
