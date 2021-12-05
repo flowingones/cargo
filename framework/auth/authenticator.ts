bc1811631afe24c049c658b506e52748a5e6bc8c
Daniel Steuri <daniel.steuri@flowingones.ch>
2021-12-05 15:02:06 +0000

    Add first implementation of auth framework

---
+++ framework/auth/authenticator.ts
@@ -0,0 +1,50 @@
+import { RequestContext } from "../core/mod.ts";
+import { Middleware, Next } from "../core/middleware/middleware.ts";
+import { UnauthorizedException } from "../core/exceptions/mod.ts";
+
+export interface Instructions<T> {
+  allow: (entity: T) => void;
+  deny: (reason: string) => void;
+}
+
+export interface Strategy<T> {
+  name: string;
+  authenticate: (ctx: RequestContext, instructions: Instructions<T>) => void;
+}
+
+function authentication<T>(
+  strategy: Strategy<T>,
+  ctx: RequestContext,
+): Promise<T> {
+  return new Promise<T>((resolve, reject) => {
+    strategy.authenticate(ctx, { allow: resolve, deny: reject });
+  });
+}
+
+const strategies: Strategy<unknown>[] = [];
+
+function protectWith<T>(strategyName: string): Middleware {
+  const strategy = <Strategy<T> | undefined> (
+    strategies.find((strategy) => strategy.name == strategyName)
+  );
+  if (strategy) {
+    return async (ctx: RequestContext, next: Next): Promise<Response> => {
+      try {
+        ctx.auth = await authentication<T>(strategy, ctx);
+      } catch (message) {
+        throw new UnauthorizedException(message);
+      }
+      return next(ctx);
+    };
+  }
+  throw Error("Strategy not defined!");
+}
+
+function register<T>(strategy: Strategy<T>) {
+  strategies.push(strategy);
+}
+
+export const Authenticator = {
+  protectWith,
+  register,
+};
---
+++ framework/auth/mod.ts
@@ -0,0 +1,3 @@
+export * from "./authenticator.ts";
+
+export * from "./strategies/mod.ts";
