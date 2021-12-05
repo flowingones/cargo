ecdef020466d51786c9db3de1bb6d748c422e125
Daniel Steuri <daniel.steuri@flowingones.ch>
2021-12-05 15:14:44 +0000

    - Add comments - Update import statement

--- examples/auth.ts
+++ examples/auth.ts
@@ -1,10 +1,16 @@
 import { bootstrap, Get } from "https://deno.land/x/cargo/core/mod.ts";
-import { Authenticator, CustomStrategy } from "https://deno.land/x/auth/mod.ts";
+import {
+  Authenticator,
+  CustomStrategy,
+} from "https://deno.land/x/cargo/auth/mod.ts";
 
 interface AuthenticatedUser {
   givenName: string;
 }
 
+/*
+ * 1. Add strategy to the Authenticator
+ */
 Authenticator.register(
   new CustomStrategy<AuthenticatedUser>((_ctx, { allow, deny }) => {
     const givenName = (<{ message: string }> _ctx.params).message;
@@ -16,9 +22,16 @@ Authenticator.register(
   }),
 );
 
+/*
+ * Register a route
+ */
 Get("/:message", ({ auth }) => {
   return new Response(`${(<AuthenticatedUser> auth).givenName}`);
-}).middleware(Authenticator.protectWith("Custom"));
+})
+  /*
+   * 3. Protect route with strategy
+   */
+  .middleware(Authenticator.protectWith("Custom"));
 
 /*
  * 4. Bootstrap and Run the Application
