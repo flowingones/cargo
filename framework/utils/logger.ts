144b42131f6c97c9ba94bb65aac323ffd0f07fac
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-13 21:04:05 +0000

    Solve issue with logger color styling in deno version <1.17.x

--- framework/utils/logger.ts
+++ framework/utils/logger.ts
@@ -2,9 +2,14 @@ import { NAME } from "../constants.ts";
 
 export function log(context: string, message: string) {
   console.log(
-    `%c(${NAME}) %c${context} %c${message}`,
-    "color:#12DC00; font-weight: bold",
-    "font-weight: bold",
-    "color: inital",
+    `${bold(green(NAME))} ${bold(context)} ${message}`,
   );
+}
+
+function green(text: string) {
+  return `\x1b[32m${text}\x1b[0m`;
+}
+
+function bold(text: string) {
+  return `\x1b[1m${text}\x1b[0m`;
 }
