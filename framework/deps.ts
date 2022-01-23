733fc45808f43d022c2ab8a8f1f42f51d361d035
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-23 19:51:42 +0000

    Update dependencies versions

--- framework/deps.ts
+++ framework/deps.ts
@@ -1,2 +1,2 @@
-export { serve } from "https://deno.land/std@0.121.0/http/server.ts";
-export { extname } from "https://deno.land/std@0.121.0/path/mod.ts";
+export { serve } from "https://deno.land/std@0.122.0/http/server.ts";
+export { parse } from "https://deno.land/std@0.122.0/path/mod.ts";
