ab24409c85316a50abf7df1e5a9caa0b7f1cd4de
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-31 17:24:52 +0000

    WIP: Error bug

--- framework/middleware/validate.ts
+++ framework/middleware/validate.ts
@@ -1,23 +1,21 @@
-import type { RequestContext } from "../http/mod.ts";
 import { BadRequestException } from "../http/mod.ts";
 import { Next } from "./middleware.ts";
 
 import { BaseSchema } from "../deps.ts";
 
+class ValidationException extends BadRequestException {
+  constructor(public error: string[]) {
+    super();
+  }
+}
+
 export function validateBody(schema: BaseSchema) {
-  return (ctx: RequestContext, next: Next) => {
-    const errors = schema.validate(ctx.body, "Request Body").errors;
+  return (ctx: unknown, next: Next) => {
+    const errors = schema.validate((<Request> ctx).body, "Request Body").errors;
     if (errors) {
       throw new ValidationException(errors.map((error) => {
         return `${error.message}`;
       }));
     }
-    return next(ctx);
   };
-}
-
-class ValidationException extends BadRequestException {
-  constructor(public error: string[]) {
-    super();
-  }
 }
