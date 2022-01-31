c247188f1457b5045b1291622b340d206473db52
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-31 18:12:57 +0000

    WIP: Error bux fix

--- framework/middleware/validate.ts
+++ framework/middleware/validate.ts
@@ -1,21 +1,16 @@
-import { BadRequestException } from "../http/mod.ts";
+import { BadRequestException, RequestContext } from "../http/mod.ts";
 import { Next } from "./middleware.ts";
 
 import { BaseSchema } from "../deps.ts";
 
-export class ValidationException extends BadRequestException {
-  constructor(public error: string[]) {
-    super();
-  }
-}
-
 export function validateBody(schema: BaseSchema) {
-  return (ctx: unknown, next: Next) => {
-    const errors = schema.validate((<Request> ctx).body, "Request Body").errors;
+  return (ctx: RequestContext, next: Next) => {
+    const errors = schema.validate(ctx.body, "Request Body").errors;
     if (errors) {
-      throw new ValidationException(errors.map((error) => {
+      throw new BadRequestException().error = errors.map((error) => {
         return `${error.message}`;
-      }));
+      });
     }
+    return next(ctx);
   };
 }
