c56dba6b2146bc900b15b2db3d9ccafc35c4d46e
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-01-31 17:13:40 +0000

    WIP: Remove Exception

--- framework/middleware/validate.ts
+++ framework/middleware/validate.ts
@@ -3,19 +3,10 @@ import { Next } from "./middleware.ts";
 
 import { BaseSchema } from "../deps.ts";
 
-class ValidationException extends BadRequestException {
-  constructor(public error: string[]) {
-    super();
-  }
-}
-
 export function validateBody(schema: BaseSchema) {
   return (ctx: RequestContext, next: Next) => {
     const errors = schema.validate(ctx.body, "Request Body").errors;
     if (errors) {
-      throw new ValidationException(errors.map((error) => {
-        return `${error.message}`;
-      }));
     }
     return next(ctx);
   };
