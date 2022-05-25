124bcc1b0f2b94c72b60914a771a8df75115a586
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-05-25 12:25:06 +0000

    Add deps for managing dependenies of the examples

---
+++ examples/deps.ts
@@ -0,0 +1,13 @@
+export { bootstrap } from "https://deno.land/x/cargo@0.1.35/mod.ts";
+
+export {
+  Get,
+  type UrlParams,
+} from "https://deno.land/x/cargo@0.1.35/http/mod.ts";
+
+export {
+  Authenticator,
+  LocalStrategy,
+} from "https://deno.land/x/cargo@0.1.35/auth/mod.ts";
+
+export { timeToResponse } from "https://deno.land/x/cargo@0.1.35/middleware/mod.ts";
