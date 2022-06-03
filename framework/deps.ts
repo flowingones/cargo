6054bfd12ffe633e322862b3d9f8d1104dcc83a8
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-03 17:27:58 +0000

    Upgrade to Cargo Version 0.1.39

--- framework/deps.ts
+++ framework/deps.ts
@@ -1,4 +1,4 @@
 export { serve } from "https://deno.land/std@0.123.0/http/server.ts";
 export { parse } from "https://deno.land/std@0.123.0/path/mod.ts";
 
-export { BaseSchema } from "https://deno.land/x/cargo_inspect@0.1.38/mod.ts";
+export { BaseSchema } from "https://deno.land/x/cargo_inspect@0.1.39/mod.ts";
