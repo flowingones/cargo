7cfcabbba9389b4c2158d3af4a87c57b1b4cdacb
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-09 18:42:56 +0000

    Upgrade to Cargo 0.1.40

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,13 +1,13 @@
-export { bootstrap } from "https://deno.land/x/cargo@0.1.39/mod.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.40/mod.ts";
 
 export {
   Get,
   type UrlParams,
-} from "https://deno.land/x/cargo@0.1.39/http/mod.ts";
+} from "https://deno.land/x/cargo@0.1.40/http/mod.ts";
 
 export {
   Authenticator,
   LocalStrategy,
-} from "https://deno.land/x/cargo@0.1.39/auth/mod.ts";
+} from "https://deno.land/x/cargo@0.1.40/auth/mod.ts";
 
-export { timeToResponse } from "https://deno.land/x/cargo@0.1.39/middleware/mod.ts";
+export { timeToResponse } from "https://deno.land/x/cargo@0.1.40/middleware/mod.ts";
