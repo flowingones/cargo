42fc74043cb472604d67e15e6bca8cc0a2740856
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-20 21:47:22 +0000

    Fix validation middleware

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,12 +1,12 @@
 import { BadRequestException, RequestContext } from "../http/mod.ts";
 import { Next } from "./middleware.ts";
 
-import { BaseSchema } from "../deps.ts";
+import { BaseSchema } from "../../inspect/schema.ts";
 
 export function validateBody(schema: BaseSchema) {
   return (ctx: RequestContext, next: Next) => {
     const errors = schema.validate(ctx.body, "Request Body").errors;
-    if (errors) {
+    if (errors.length) {
       const exception = new BadRequestException();
       exception.error = errors.map((error) => {
         return `${error.message}`;
