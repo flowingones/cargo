48bf878447ef3ed8b756418b3c0728a873250739
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-20 21:46:23 +0000

    Add 413 http error

---
+++ src/http/exceptions/entity-too-large-exception.ts
@@ -0,0 +1,8 @@
+import { HttpStatus } from "../mod.ts";
+import { HttpException } from "./http-exception.ts";
+
+export class EntityTooLargeException extends HttpException {
+  constructor(public message: string = "Request Entity Too Large") {
+    super(message, HttpStatus.ENTITY_TOO_LARGE);
+  }
+}
