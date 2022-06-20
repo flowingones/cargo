97a6f2fd7239445f83681c9e73958fb6c14a24c3
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-06-20 21:47:47 +0000

    Rearrange imports

--- src/middleware/mod.ts
+++ src/middleware/mod.ts
@@ -1,6 +1,6 @@
+export * from "./body-parser/body-parser.ts";
 export * from "./middleware.ts";
-
-export * from "./time-to-response.ts";
-export * from "./search-params.ts";
 export * from "./raw-body.ts";
+export * from "./search-params.ts";
+export * from "./time-to-response.ts";
 export * from "./validate.ts";
