832905c3e32ec1eec9ec6fa6df93778510091b7f
Daniel Steuri <danielsteuri@daniels-macbook-pro.local>
2022-09-25 20:19:34 +0000

    style: reformat code

--- src/http/exceptions/internal-server-exception.ts
+++ src/http/exceptions/internal-server-exception.ts
@@ -2,7 +2,9 @@ import { HttpStatus } from "../mod.ts";
 import { HttpException } from "./http-exception.ts";
 
 export class InternalServerException extends HttpException {
-  constructor(public message: string = "Internal Server Error") {
+  constructor(
+    public message: string = "Internal Server Error",
+  ) {
     super(message, HttpStatus.INTERAL_ERROR);
   }
 }
--- src/http/exceptions/unauthorized-exception.ts
+++ src/http/exceptions/unauthorized-exception.ts
@@ -2,7 +2,9 @@ import { HttpStatus } from "../http-status.ts";
 import { HttpException } from "./http-exception.ts";
 
 export class UnauthorizedException extends HttpException {
-  constructor(public message: string = "Unauthorized") {
+  constructor(
+    public message: string = "Unauthorized",
+  ) {
     super(message, HttpStatus.UNAUTHORIZED);
   }
 }
