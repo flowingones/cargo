aae4d03b6e61b24a8498ee639808c72e374ffcf9
Daniel Steuri <danielsteuri@daniels-macbook-pro.local>
2022-09-25 18:31:36 +0000

    chore: raise version of cargo

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,15 +1,15 @@
-export { bootstrap } from "https://deno.land/x/cargo@0.1.43/mod.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.44/mod.ts";
 
 export {
   autoloadAssets,
   autoloadRoutes,
   Get,
   type UrlParams,
-} from "https://deno.land/x/cargo@0.1.43/http/mod.ts";
+} from "https://deno.land/x/cargo@0.1.44/http/mod.ts";
 
 export {
   Authenticator,
   LocalStrategy,
-} from "https://deno.land/x/cargo@0.1.43/auth/mod.ts";
+} from "https://deno.land/x/cargo@0.1.44/auth/mod.ts";
 
-export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.43/middleware/mod.ts";
+export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.44/middleware/mod.ts";
