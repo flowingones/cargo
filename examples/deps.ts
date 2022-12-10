071313924e6eb43451f7cbd5557c1572736e30d4
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-12-10 20:43:00 +0000

    chore: bump cargo version

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,15 +1,15 @@
-export { bootstrap } from "https://deno.land/x/cargo@0.1.47/mod.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.48/mod.ts";
 
 export {
   autoloadAssets,
   autoloadRoutes,
   Get,
   type UrlParams,
-} from "https://deno.land/x/cargo@0.1.47/http/mod.ts";
+} from "https://deno.land/x/cargo@0.1.48/http/mod.ts";
 
 export {
   Authenticator,
   LocalStrategy,
-} from "https://deno.land/x/cargo@0.1.47/auth/mod.ts";
+} from "https://deno.land/x/cargo@0.1.48/auth/mod.ts";
 
-export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.47/middleware/mod.ts";
+export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.48/middleware/mod.ts";
--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-// Cargo – Version 0.1.47
+// Cargo – Version 0.1.48
 export * from "./app.ts";
 export * from "./constants.ts";
 export * from "./tasks.ts";
