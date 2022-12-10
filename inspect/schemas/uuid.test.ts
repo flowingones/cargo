2c35ba2e666618bb793acfcb91a44899eb13236c
Daniel Steuri <daniel.steuri@flowingones.ch>
2022-12-10 20:38:19 +0000

    feat: add validator for uuid1 and uuid 4

---
+++ inspect/schemas/uuid.test.ts
@@ -0,0 +1,49 @@
+import { assertEquals } from "./deps_tests.ts";
+import { UUIDSchema } from "./uuid.ts";
+
+const notUUIDMessage = '"string" is not a valid "UUID"';
+const uuid1 = "ae6ad8ac-78c7-11ed-a1eb-0242ac120002";
+const uuid4 = "a05dfb70-7359-45a8-8407-45f509b24258";
+
+Deno.test("UUID Schema Validation: 'isUUID'", async (t) => {
+  await t.step("should validated all UUIDs", () => {
+    const uuidSchema = new UUIDSchema();
+    assertEquals(uuidSchema.validate(uuid1).errors, []);
+    assertEquals(uuidSchema.validate(uuid4).errors, []);
+    assertEquals(uuidSchema.validate("Cargo").errors, [{
+      message: notUUIDMessage,
+    }]);
+  });
+
+  await t.step("should validated all UUIDs and allow undefined", () => {
+    const uuidSchema = new UUIDSchema().optional();
+    assertEquals(uuidSchema.validate(undefined).errors, []);
+    assertEquals(uuidSchema.validate(uuid1).errors, []);
+    assertEquals(uuidSchema.validate(uuid4).errors, []);
+    assertEquals(uuidSchema.validate("Cargo").errors, [{
+      message: notUUIDMessage,
+    }]);
+  });
+
+  await t.step("should validated UUID V1", () => {
+    const uuidSchema = new UUIDSchema("1");
+    assertEquals(uuidSchema.validate(uuid1).errors, []);
+    assertEquals(uuidSchema.validate(uuid4).errors, [{
+      message: notUUIDMessage,
+    }]);
+    assertEquals(uuidSchema.validate("Cargo").errors, [{
+      message: notUUIDMessage,
+    }]);
+  });
+
+  await t.step("should validated UUID V4", () => {
+    const uuidSchema = new UUIDSchema("4");
+    assertEquals(uuidSchema.validate(uuid1).errors, [{
+      message: notUUIDMessage,
+    }]);
+    assertEquals(uuidSchema.validate(uuid4).errors, []);
+    assertEquals(uuidSchema.validate("Cargo").errors, [{
+      message: notUUIDMessage,
+    }]);
+  });
+});
---
+++ inspect/schemas/uuid.ts
@@ -0,0 +1,32 @@
+import { PrimitiveSchema, ValidationError } from "../mod.ts";
+
+export type Version = "1" | "4" | "all";
+
+const patterns: Record<Version, RegExp> = {
+  all: /^[0-9A-Z]{8}(-[0-9A-Z]{4}){3}-[0-9A-Z]{12}$/i,
+  1: /^[0-9A-F]{8}-[0-9A-F]{4}-1[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  // 2: /^[0-9A-F]{8}-[0-9A-F]{4}-2[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  // 3: /^[0-9A-F]{8}-[0-9A-F]{4}-3[0-9A-F]{3}-[0-9A-F]{4}-[0-9A-F]{12}$/i,
+  4: /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+  // 5: /^[0-9A-F]{8}-[0-9A-F]{4}-5[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i,
+};
+
+export class UUIDSchema extends PrimitiveSchema {
+  constructor(version?: Version) {
+    super("uuid");
+    this.validator(isUUID(version));
+  }
+}
+
+function isUUID(
+  version?: Version,
+) {
+  return (value: unknown, key?: string): ValidationError | undefined => {
+    if (typeof value === "string" && patterns[version || "all"].test(value)) {
+      return;
+    }
+    return {
+      message: `"${key || "string"}" is not a valid "UUID"`,
+    };
+  };
+}
