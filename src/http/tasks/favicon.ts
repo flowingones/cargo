f8357c7e00510769eb7a1b63af57bd8620a92f73
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-03-19 23:11:35 +0000

    feat: set max-age in prod

--- src/http/tasks/favicon.ts
+++ src/http/tasks/favicon.ts
@@ -1,3 +1,5 @@
+import { isProd } from "../../utils/environment.ts";
+
 /**
  * Task to load a favicon from the provided path
  * and register a route (/favicon.ico) to it.
@@ -5,27 +7,28 @@
  */
 export function Favicon(path: string) {
   return (app: any) => {
-	app.getProtocol("http")?.router.add({
-	  path: "/favicon.ico",
-	  method: "GET",
-	  handler: async () => {
-		try {
-		  const file = await Deno.open(path);
-		  return new Response(
-			file.readable,
-			{
-			  headers: {
-				"content-type": "image/vnd.microsoft.icon",
-			  },
-			},
-		  );
-		} catch (e) {
-		  if (e instanceof Deno.errors.NotFound) {
-			throw new Error("Not able to load favicon");
-		  }
-		  throw e;
-		}
-	  },
-	});
+    app.getProtocol("http")?.router.add({
+      path: "/favicon.ico",
+      method: "GET",
+      handler: async () => {
+        try {
+          const file = await Deno.open(path);
+          return new Response(
+            file.readable,
+            {
+              headers: {
+                "content-type": "image/vnd.microsoft.icon",
+                ...(isProd() ? { "cache-control": "max-age=3600" } : {}),
+              },
+            },
+          );
+        } catch (e) {
+          if (e instanceof Deno.errors.NotFound) {
+            throw new Error("Not able to load favicon");
+          }
+          throw e;
+        }
+      },
+    });
   };
 }
