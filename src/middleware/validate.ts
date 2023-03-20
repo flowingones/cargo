3131d9aba99ceb84a8eec92eaedb0f27248095e3
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-03-20 11:25:12 +0000

    fix: fix type error

--- src/middleware/validate.ts
+++ src/middleware/validate.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, RequestContext } from "../http/mod.ts";
 import { Next } from "./middleware.ts";
 
-import { BaseSchema } from "../deps.ts";
+import { BaseSchema } from "inspect/mod.ts";
 
 export function validateBody(schema: BaseSchema) {
   return (ctx: RequestContext, next: Next) => {
