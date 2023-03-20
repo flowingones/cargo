e3e134c1cffcdc32a129b45e624ee18db0a1193b
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-03-20 19:38:39 +0000

    chore: upgrade cargo version

--- inspect/mod.ts
+++ inspect/mod.ts
@@ -1,3 +1,3 @@
-// Cargo Inspect – Version 0.1.49
+// Cargo Inspect – Version 0.1.50
 export * from "./schema.ts";
 export * from "./schemas/mod.ts";
--- src/http/mod.ts
+++ src/http/mod.ts
@@ -1,4 +1,4 @@
-// Cargo/http – Version 0.1.49
+// Cargo/http – Version 0.1.50
 export * from "./exceptions/mod.ts";
 export * from "./http-method.ts";
 export * from "./http-status.ts";
