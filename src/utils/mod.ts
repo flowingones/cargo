6a5e135b09aa12a372231ac9cfa1a8f78bbaf15f
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-03-20 19:35:47 +0000

    chore: export environment in mod.ts

--- src/utils/mod.ts
+++ src/utils/mod.ts
@@ -1,5 +1,6 @@
-// Cargo/utils – Version 0.1.49
+// Cargo/utils – Version 0.1.50
 export * from "./file.ts";
 export * from "./logger.ts";
 export * from "./mime-types.ts";
 export * from "./options.ts";
+export * from "./environment.ts";
