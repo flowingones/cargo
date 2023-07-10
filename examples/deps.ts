8a48b5e6fab24250ea130779edb24b8f3fd673cf
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-07-10 23:05:44 +0200

    chore: raise cargo version

--- examples/deps.ts
+++ examples/deps.ts
@@ -1,13 +1,13 @@
-export { bootstrap } from "https://deno.land/x/cargo@0.1.51/mod.ts";
+export { bootstrap } from "https://deno.land/x/cargo@0.1.52/mod.ts";
 
 export {
   Get,
   type UrlParams,
-} from "https://deno.land/x/cargo@0.1.51/http/mod.ts";
+} from "https://deno.land/x/cargo@0.1.52/http/mod.ts";
 
 export {
   Authenticator,
   LocalStrategy,
-} from "https://deno.land/x/cargo@0.1.51/auth/mod.ts";
+} from "https://deno.land/x/cargo@0.1.52/auth/mod.ts";
 
-export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.51/middleware/mod.ts";
+export { logTimeToResponse } from "https://deno.land/x/cargo@0.1.52/middleware/mod.ts";
--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-// Cargo – Version 0.1.51
+// Cargo – Version 0.1.52
 export * from "./app.ts";
 export * from "./constants.ts";
 export * from "./tasks.ts";
