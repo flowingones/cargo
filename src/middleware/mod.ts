a6071dea3f4650aa7bf7ee4f25939c4f17537f06
Daniel Steuri <daniel.steuri@flowingones.ch>
2023-08-20 11:48:56 +0200

    chore: upgrade cargo version

--- src/middleware/mod.ts
+++ src/middleware/mod.ts
@@ -1,4 +1,4 @@
-// Cargo/middleware – Version 0.1.54
+// Cargo/middleware – Version 0.1.55
 export { bodyParser } from "./body-parser/body-parser.ts";
 export * from "./middleware.ts";
 export { addRawBodyToContext } from "./add-raw-body-to-context.ts";
--- src/mod.ts
+++ src/mod.ts
@@ -1,4 +1,4 @@
-// Cargo – Version 0.1.54
+// Cargo – Version 0.1.55
 export * from "./app.ts";
 export * from "./constants.ts";
 export * from "./protocol.ts";
